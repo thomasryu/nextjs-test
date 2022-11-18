feff4800507bbab02e40aa1b608e0df30f2a50b4
thomasryu <thomas.sameshima@gmail.com>
2022-11-18 14:51:40 -0300

    Separating home links

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import Link from 'next/link'
 const Home = () => (
   <div>
     Trailing slash
+    <br />
+    <br />
     <Link href="/about/">Link with trailing slash</Link>
+    <br />
+    <br />
     <Link href="/about">Link without trailing slash</Link>
   </div>
 )
